539326e70de8d1fd97367ce5c662f92c5161d219
Kvazar-213452 <kvazar382@gmail.com>
2025-01-27 19:31:08 +0200

    fix

--- head_com/app_func.go
+++ head_com/app_func.go
@@ -1,1 +1,14 @@
 package head_com
+
+import (
+	"fmt"
+	"os"
+)
+
+func Read_file(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", fmt.Errorf("error %s: %v", filename, err)
+	}
+	return string(data), nil
+}
