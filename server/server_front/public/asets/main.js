3187bb21c016de1e58fe4a45484b41062060fa7f
Kvazar-213452 <kvazar382@gmail.com>
2025-01-31 00:30:30 +0200

    css

--- server/server_front/public/asets/main.js
+++ server/server_front/public/asets/main.js
@@ -10,10 +10,10 @@ function upload_foto() {
         contentType: false,
         processData: false,
         success: function(response) {
-            $(".console").append("good")
+            $(".console").append("<p>good</p>")
         },
         error: function(xhr, status, error) {
-            $(".console").append("error dwn folo")
+            $(".console").append("<p>error dwn folo</p>")
         }
     });
 }
@@ -30,10 +30,38 @@ function upload_music() {
       contentType: false,
       processData: false,
       success: function(response) {
-        $(".console").append("good")
+        $(".console").append("<p>good</p>")
       },
       error: function(xhr, status, error) {
-        $(".console").append("error dwn music")
+        $(".console").append("<p>error dwn music</p>")
       }
+    });
+}
+
+function get_data() {
+    $(".console").append("<p>get_data</p>")
+
+    $.get('/music', function(data) {
+        $("#data_music").html(null)
+
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                let text = `<div class="item"><p>del</p> ${key}: <span>${data[key]}</span></div>`;
+
+                $("#data_music").append(text)
+            }
+        }
+    });
+
+    $.get('/foto', function(data) {
+        $("#data_foto").html(null)
+
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                let text = `<div class="item"><p>del</p> ${key}: <span>${data[key]}</span></div>`;
+
+                $("#data_foto").append(text)
+            }
+        }
     });
 }
