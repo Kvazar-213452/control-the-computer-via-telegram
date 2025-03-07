0e9cd2d22968c8ece72a3a38741233503dccf7fd
Conar365 <kvazar382@gmail.com>
2025-03-07 20:47:12 +0300

    C#

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// Program.cs
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Telegram.Bot;
+using MyTelegramBot;
+
+class Program {
+    static async Task Main(string[] args) {
+        string json = File.ReadAllText("token.json");
+        dynamic tokenData = JsonConvert.DeserializeObject(json);
+        string token = tokenData.token;
+
+        var botClient = new TelegramBotClient(token);
+
+        var me = await botClient.GetMeAsync();
+        Console.WriteLine($"Hello! I am {me.Username}!");
+
+        BotHandler handler = new BotHandler(botClient);
+
+        botClient.OnMessage += handler.HandleMessages;
+
+        botClient.StartReceiving();
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
+        botClient.StopReceiving();
+    }
+}
---
+++ src/BotHandler.cs
@@ -0,0 +1,27 @@
+// src/BotHandler.cs
+using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace MyTelegramBot {
+    public class BotHandler {
+        private TelegramBotClient botClient;
+
+        public BotHandler(TelegramBotClient client) {
+            botClient = client;
+        }
+
+        public async void HandleMessages(object sender, MessageEventArgs e) {
+            if (e.Message.Text != null) {
+                Console.WriteLine($"Received a message from {e.Message.From.Username}: {e.Message.Text}");
+
+                // Перевірка на команду #help
+                if (e.Message.Text.StartsWith("#help")) {
+                    await botClient.SendTextMessageAsync(e.Message.Chat.Id, "Here is the list of commands:\n#help - Shows this help message\n...");
+                } else {
+                    await botClient.SendTextMessageAsync(e.Message.Chat.Id, "You said: " + e.Message.Text);
+                }
+            }
+        }
+    }
+}
